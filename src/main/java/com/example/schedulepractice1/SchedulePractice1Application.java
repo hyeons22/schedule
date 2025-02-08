09f58ba9173e388aa9770fe7a352ce2da2506b4f
hyeon <ski0123@naver.com>
2025-02-08 12:19:58 +0900

    first commit

---
+++ src/main/java/com/example/schedulepractice1/SchedulePractice1Application.java
@@ -0,0 +1,13 @@
+package com.example.schedulepractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulePractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulePractice1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulepractice1/SchedulePractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulepractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulePractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
